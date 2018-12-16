b4949696d899f9fb76edf1c9b7a67cc20cb26357
abelQJ <zglieren@126.com>
2018-12-16 17:45:15 +0800

    update

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
     "log"
     "path/filepath"
     "flag"
-    "expvar"
+	"expvar"
+	"fmt"
 )
 
 func init(){
@@ -15,8 +16,10 @@ func init(){
 }
 
 func main(){
-    var confAccessKey string
-    flag.StringVar(&confAccessKey , "confAccessKey" , "pi" ,"-confAccessKey key")
+    var confAccessKey,port,https string
+	flag.StringVar(&confAccessKey , "confAccessKey" , "pi" ,"-confAccessKey key")
+	flag.StringVar(&port , "port" , "8080" ,"-port num")
+	flag.StringVar(&https , "https" , "false" ,"-https true|false")
     flag.Parse()
     expvar.Get("confAccessKey").(*expvar.String).Set(confAccessKey)
     http.Handle("/", player.NotFoundPage{})
@@ -31,9 +34,16 @@ func main(){
     http.Handle("/cgi/cancelLoveVideo" , player.CancelLoveVideoCgi{})
     http.Handle("/cgi/hateVideo" , player.HateVideoCgi{})
     http.Handle("/cgi/cancelHateVideo" , player.CancelHateVideoCgi{})
-    http.Handle("/resources/" , http.FileServer(http.Dir(filepath.Join(".","static"))))
-    err := http.ListenAndServe(":8116", nil)
-    //err := http.ListenAndServeTLS(":9090", "./ca/server.crt","./ca/server.key", nil)
+	http.Handle("/resources/" , http.FileServer(http.Dir(filepath.Join(".","static"))))
+	listenPort := fmt.Sprintf(":%s",port)
+	var err error
+	if https == "true" {
+	  serverCrt := filepath.Join(".","ca","server.crt")
+	  serverKey := filepath.Join(".","ca","server.key")
+      err = http.ListenAndServeTLS(listenPort, serverCrt , serverKey, nil)
+	} else {
+		err = http.ListenAndServe(listenPort, nil)
+	}
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
  }
